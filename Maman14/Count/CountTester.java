208516bb4ca7d667c8f88511b08d97d0336c704d
Eden998 <64744159+Eden998@users.noreply.github.com>
2020-05-13 12:18:53 +0300

    Update CountTester.java

--- Maman14/Count/CountTester.java
+++ Maman14/Count/CountTester.java
@@ -5,7 +5,7 @@ public class CountTester
         System.out.print(Ex14.count("eee", "e") == 3 ? "Your " : "NOT ");
         System.out.print(Ex14.count("eeee", "ee") == 6 ? "Count " : "NOT ");
         System.out.print(Ex14.count("ebeb", "e") == 2 ? "Method " : "NOT ");
-        System.out.print(Ex14.count("hellohello", "hello") == 6 ? "Works " : "NOT ");
+        System.out.print(Ex14.count("hellohello", "hello") == 9 ? "Works " : "NOT ");
         System.out.print(Ex14.count("nbnbnbnbnb", "nb") == 15 ? "Great.\n" : "NOT ");
         System.out.print(Ex14.count("Hola", "d") == 0 ? "Very " : "NOT ");
         System.out.print(Ex14.count("jdk52522522sgg", "52") == 11 ? "Good " : "NOT ");
